6c565727e424b9992e84348d74e9cd1f7e526127
Kenroy Harrison <kenroyh@gmail.com>
2013-07-21 12:25:46 +0900

    added public path

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express.createServer(express.logger());
 
@@ -17,7 +18,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
